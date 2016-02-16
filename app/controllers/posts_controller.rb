d6a5a906620761488584fdad602ecaeaaeca4177
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-02-15 23:49:27 -0800

    too tired for any sensible message. dont judge me

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,16 +11,15 @@ class PostsController < ApplicationController
 
   def show
     @reviews = Review.where(post_id: @post.id).order('created_at DESC')
-    #if @reviews.blank?
-     # @reviews = 0
-    #else
-    #  @avg_review = @reviews.average(:rating).round(2)
-    #end  
+    if @reviews.blank?
+       @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end  
   end
 
   def new
-    #@post = current_user.posts.build  
-    @post = current_user.posts.build(post_params)
+    @post = current_user.posts.build  
   end
 
   def edit
@@ -54,17 +53,24 @@ class PostsController < ApplicationController
 
   def connect
     @buyer = Buyer.new
-    @buyer.user_id = current_user.id
-    @buyer.post_id = @post.id
-    if @buyer.save
-	@post.increment(:number_of_buyers, by = 1)	
-        @post.save
-	flash[:notice] = "Connection Created"
-    	redirect_to posts_path
-    else
-        flash[:alert] = "Connection Not Created"
-    end 
-  end
+    if current_user.id != @post.user_id
+        @buyer.user_id = current_user.id
+        @buyer.post_id = @post.id
+        if @buyer.save
+	  @post.increment(:number_of_buyers, by = 1)	
+          if @post.save
+	    flash[:notice] = "Connection Created"
+    	    redirect_to posts_path
+    	  else
+            flash[:alert] = "Connection Not Created"
+          end
+        else
+            flash[:alert] = "Connection Not Created"
+        end 
+   else
+     flash[:alert] = "Connection Not Created"
+   end 
+ end
 
   private 
 
