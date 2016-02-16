c71209ab9e4a758b02d1a413bed9a0ac71d70045
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-02-15 23:22:24 -0800

    Updated with better styling

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,4 +1,5 @@
 class Buyer < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+  validates :post_id, uniqueness: { scope: :user_id, message: "You've already expressed interest in this post!" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,14 +1,16 @@
 class Review < ActiveRecord::Base
 
+	belongs_to :user
+        belongs_to :post
+	# post will have many reviews
+
 	validates :reviewee_id, presence: true
 	validates :user_id, presence: true
 	validates :post_id, presence: true
 	validates :title, presence: true
 	validates :rating, presence: true
 	validates :description, presence: true
-	belongs_to :user
-        belongs_to :post
  	validates_uniqueness_of :title
-	# post will have many reviews
+	validates :post_id, uniqueness: { scope: :user_id, message: "You've already reviewed this post!" }
 
 end
