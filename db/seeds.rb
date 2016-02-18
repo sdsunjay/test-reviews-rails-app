4b50181a50b6a03cefcc111812fafd8d016496a1
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-02-17 23:51:51 -0800

    Added another user and removed a post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 user1 = User.create :email => "user1@name.com", :password => "password"
 user2 = User.create :email => "user2@name.com", :password => "password"
 user3 = User.create :email => "user3@name.com", :password => "password"
+user4 = User.create :email => "user4@name.com", :password => "password"
 
-posts = Post.create ([{user_id: 1, title: "test1"},{user_id: 2, title: "test2"},{user_id: 1, title: "test3"}])
+posts = Post.create ([{user_id: 2, title: "test1"},{user_id: 3, title: "test2"}])
 
